300eae791c252b8d968f163431bc56814b12229b
student <o1103025@gm.pu.edu.tw>
2023-03-22 13:52:50 +0800

    第二次(ONCLICK)

--- app/src/main/java/tw/edu/pu/gm/o1103025/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/o1103025/MainActivity.kt
@@ -2,10 +2,23 @@ package tw.edu.pu.gm.o1103025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            if( txv.text == "你好") {
+                txv.text = "海青班"
+            }else{
+                txv.text= "你們好"
+                }
+
+            })
     }
 }
